4ddcb00f9dac0aaaa4c642ddb2a71104d55c407e
Ahsan Minhas <ahsan.minhas.official@gmail.com>
2026-05-28 16:51:20 +0500

    Initial Commit

---
+++ Raven/RavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavenApp.swift
+//  Raven
+//
+//  Created by Ahsan Minhas on 28/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
